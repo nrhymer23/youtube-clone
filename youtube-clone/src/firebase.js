c01afab27be00e56804487586a96abc5db64cf02
Created unity Project <nrhymer@students.kennesaw.edu>
2021-01-19 15:38:02 -0500

    Basic project setup and file clean up

---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD13Qt7-crwa6bFa-efS84-AZ_hk7G1Y-A",
+    authDomain: "clone-8ef48.firebaseapp.com",
+    projectId: "clone-8ef48",
+    storageBucket: "clone-8ef48.appspot.com",
+    messagingSenderId: "1052509474494",
+    appId: "1:1052509474494:web:6acc32fdbaa977c2c6d8ef",
+    measurementId: "G-TH2K4E21M4"
+  };
