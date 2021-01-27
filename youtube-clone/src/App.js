f783eea4359bf1c70f249e54cc7c089cbd378386
Created unity Project <nrhymer@students.kennesaw.edu>
2021-01-27 15:59:23 -0500

    created components and styled with css flexbox.

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./Header";
 import "./App.css";
 
 
@@ -6,7 +7,14 @@ function App() {
   return (
     // BEM class naiming convention
     <div className="App">
-    <h1> Building Youtube clone</h1>
+    {/* <h1> Building Youtube clone</h1> */}
+    <Header />
+
+    {/* Header */ }
+    {/* Sidebar */ }
+    {/* RecommendedVideo */ }
+
+
     </div>
   );
 }
---
+++ youtube-clone/src/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Header.css';
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Avatar from "@material-ui/core/Avatar";
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+            <MenuIcon />
+            <img 
+                className="header__logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1280px-YouTube_Logo_2017.svg.png" 
+                alt="" 
+                />
+                </div>
+
+                <div className="header__input">
+                <input placeholder="Search" type="text" />
+                <SearchIcon className="header__inputButton" />
+                </div>
+
+                <div className="header__icons">
+                    <VideoCallIcon className="header__icon"/>
+                    <AppsIcon className="header__icon"/>
+                    <NotificationsIcon className="header__icon"/>
+                    <Avatar 
+                    alt="N0 LZz"
+                    src=""
+                    />
+                    </div>
+        </div>
+    )
+}
+
+export default Header
