439e9cef6fb2501b566c4db48408c188c6127458
Created unity Project <nrhymer@students.kennesaw.edu>
2021-02-05 19:44:52 -0500

    Made the Header stick to the top of page.

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -9,6 +9,28 @@ function App() {
     <div className="App">
     {/* <h1> Building Youtube clone</h1> */}
     <Header />
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      <h1>TesT</h1>
+      
+      
+    
+      
+
 
     {/* Header */ }
     {/* Sidebar */ }
