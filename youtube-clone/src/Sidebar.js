2fa9e83a07c571ff0314c08e64080052d01ccf63
Created unity Project <nrhymer@students.kennesaw.edu>
2021-02-10 17:15:40 -0500

    updated project Error Fixed

--- youtube-clone/src/Sidebar.js
+++ youtube-clone/src/Sidebar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import SidebarRow from "./SidebarRow";
-import WhatshotIcon from "@material-ui/icons/Whatshot";
-import SubscriptionIcon from "@material-ui/icons/Subscriptions";
-import HomeIcon from "@material-ui/icons/Home";
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import SubscriptionIcon from '@material-ui/icons/Subscriptions';
+import HomeIcon from '@material-ui/icons/Home';
 import "./Sidebar.css";
 
 function Sidebar() {
@@ -12,7 +12,7 @@ function Sidebar() {
             <SidebarRow Icon={HomeIcon} title="Home"/>
             <SidebarRow Icon={WhatshotIcon} title="Trending"/>
             <SidebarRow Icon={SubscriptionIcon} title="Subscription"/>
-            <SidebarRow title="Something else"/>
+            
            
         </div>
     );
