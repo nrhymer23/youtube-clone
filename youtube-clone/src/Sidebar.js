77de581f12a6a3e047969e02a40d9906459edb49
Created unity Project <nrhymer@students.kennesaw.edu>
2021-02-15 22:37:16 -0500

    Styling the Sidebar- Completed

--- youtube-clone/src/Sidebar.js
+++ youtube-clone/src/Sidebar.js
@@ -3,16 +3,28 @@ import SidebarRow from "./SidebarRow";
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import SubscriptionIcon from '@material-ui/icons/Subscriptions';
 import HomeIcon from '@material-ui/icons/Home';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import ThumpUpAltOutlinedIcon from '@material-ui/icons/ThumbDownAlt';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMore';
 import "./Sidebar.css";
 
 function Sidebar() {
     return (
         <div className="sidebar">
-
-            <SidebarRow Icon={HomeIcon} title="Home"/>
+            <SidebarRow selected Icon={HomeIcon} title="Home"/>
             <SidebarRow Icon={WhatshotIcon} title="Trending"/>
             <SidebarRow Icon={SubscriptionIcon} title="Subscription"/>
-            
+            <hr />
+            <SidebarRow Icon={VideoLibraryIcon} title="Library"/>
+            <SidebarRow Icon={HistoryIcon} title="History"/>
+            <SidebarRow Icon={OndemandVideoIcon} title="Your Videos"/>
+            <SidebarRow Icon={WatchLaterIcon} title="Watch Later"/>
+            <SidebarRow Icon={ThumpUpAltOutlinedIcon} title="Liked Videos"/>
+            <SidebarRow Icon={ExpandMoreOutlinedIcon} title="Show More"/>
+            <hr />
            
         </div>
     );
