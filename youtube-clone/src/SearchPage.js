abeed365a0a468e64d9ca532c5483b0bf9e99f75
Created unity Project <nrhymer@students.kennesaw.edu>
2021-02-23 02:03:31 -0500

    Creating Different search page Components

--- youtube-clone/src/SearchPage.js
+++ youtube-clone/src/SearchPage.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined';
+import ChannelRow from "./ChannelRow";
 import "./SearchPage.css";
 
 function SearchPage() {
     return (
         <div className="searchPage">
+            <div className="searchPage__filter">
+                <TuneOutlinedIcon />
+                <h2>FILTER</h2>
+                </div>
+        <hr></hr>
+
+        <ChannelRow
+        image="https://yt3.ggpht.com/ytc/AAUvwnjo5Cgnv1-A342q82xN0hfUbG__vNEiCI3TgN4QAg=s176-c-k-c0x00ffffff-no-rj"
+        channel="Joe Budden Tv"
+        verified
+        subs="910K"
+        noOfVideos={382}
+        description="Blah Blah Blah"
+        />
             
         </div>
     )
