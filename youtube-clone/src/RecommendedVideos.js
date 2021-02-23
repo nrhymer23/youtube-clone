c8737b1513a3c48dba0f819af143eb97f5cf1bb7
Created unity Project <nrhymer@students.kennesaw.edu>
2021-02-22 23:14:25 -0500

    Creating Video Component & filler props

--- youtube-clone/src/RecommendedVideos.js
+++ youtube-clone/src/RecommendedVideos.js
@@ -1,12 +1,64 @@
 import React from 'react';
+import VideoCard from "./VideoCard";
 import "./RecommendedVideos.css";
 
 function RecommendedVideos() {
     return (
         <div className="recommendedVideos">
-            <h2>Recommended</h2>
-            <div className="recommended__videos">
+            <h2>Recommended
+            </h2>
+
+            <div className="recommendedVideos__videos">
                 
+            <VideoCard 
+                title="BOUGHT A 25 PIECE CANON AMAZON BUNDLE, CRAZY LOW PRICE! | Is It Worth Your Money?"
+                views="32 Views "
+                timestamp="1 week ago"
+                channelImage="https://yt3.ggpht.com/ytc/AAUvwnj0xSBCAordkxhNzAzMMboWu0h3bhCYzEEhl-RExg=s176-c-k-c0x00ffffff-no-rj"
+                channel="DAYOtheGUYO"
+                image="https://i.ytimg.com/vi/qw3e-8aL9Iw/hqdefault.jpg?sqp=-oaymwEjCPYBEIoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLDynyVw-jdTzMrTiXaWUW9VM6Fsog"
+            />
+            <VideoCard 
+                 title="BOUGHT A 25 PIECE CANON AMAZON BUNDLE, CRAZY LOW PRICE! | Is It Worth Your Money?"
+                 views="32 Views "
+                 timestamp="1 week ago"
+                 channelImage="https://yt3.ggpht.com/ytc/AAUvwnj0xSBCAordkxhNzAzMMboWu0h3bhCYzEEhl-RExg=s176-c-k-c0x00ffffff-no-rj"
+                 channel="DAYOtheGUYO"
+                 image="https://i.ytimg.com/vi/qw3e-8aL9Iw/hqdefault.jpg?sqp=-oaymwEjCPYBEIoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLDynyVw-jdTzMrTiXaWUW9VM6Fsog"
+            />
+            <VideoCard 
+                 title="BOUGHT A 25 PIECE CANON AMAZON BUNDLE, CRAZY LOW PRICE! | Is It Worth Your Money?"
+                 views="32 Views "
+                 timestamp="1 week ago"
+                 channelImage="https://yt3.ggpht.com/ytc/AAUvwnj0xSBCAordkxhNzAzMMboWu0h3bhCYzEEhl-RExg=s176-c-k-c0x00ffffff-no-rj"
+                 channel="DAYOtheGUYO"
+                 image="https://i.ytimg.com/vi/qw3e-8aL9Iw/hqdefault.jpg?sqp=-oaymwEjCPYBEIoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLDynyVw-jdTzMrTiXaWUW9VM6Fsog"
+            />
+            <VideoCard 
+                 title="BOUGHT A 25 PIECE CANON AMAZON BUNDLE, CRAZY LOW PRICE! | Is It Worth Your Money?"
+                 views="32 Views "
+                 timestamp="1 week ago"
+                 channelImage="https://yt3.ggpht.com/ytc/AAUvwnj0xSBCAordkxhNzAzMMboWu0h3bhCYzEEhl-RExg=s176-c-k-c0x00ffffff-no-rj"
+                 channel="DAYOtheGUYO"
+                 image="https://i.ytimg.com/vi/qw3e-8aL9Iw/hqdefault.jpg?sqp=-oaymwEjCPYBEIoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLDynyVw-jdTzMrTiXaWUW9VM6Fsog"
+            />
+            <VideoCard 
+                 title="BOUGHT A 25 PIECE CANON AMAZON BUNDLE, CRAZY LOW PRICE! | Is It Worth Your Money?"
+                 views="32 Views "
+                 timestamp="1 week ago"
+                 channelImage="https://yt3.ggpht.com/ytc/AAUvwnj0xSBCAordkxhNzAzMMboWu0h3bhCYzEEhl-RExg=s176-c-k-c0x00ffffff-no-rj"
+                 channel="DAYOtheGUYO"
+                 image="https://i.ytimg.com/vi/qw3e-8aL9Iw/hqdefault.jpg?sqp=-oaymwEjCPYBEIoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLDynyVw-jdTzMrTiXaWUW9VM6Fsog"
+            />
+            <VideoCard 
+                 title="BOUGHT A 25 PIECE CANON AMAZON BUNDLE, CRAZY LOW PRICE! | Is It Worth Your Money?"
+                 views="32 Views "
+                 timestamp="1 week ago"
+                 channelImage="https://yt3.ggpht.com/ytc/AAUvwnj0xSBCAordkxhNzAzMMboWu0h3bhCYzEEhl-RExg=s176-c-k-c0x00ffffff-no-rj"
+                 channel="DAYOtheGUYO"
+                 image="https://i.ytimg.com/vi/qw3e-8aL9Iw/hqdefault.jpg?sqp=-oaymwEjCPYBEIoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLDynyVw-jdTzMrTiXaWUW9VM6Fsog"
+            />
+            <VideoCard />
             </div>
         </div>
     )
