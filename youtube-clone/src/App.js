6c737f425507ae0fb50c14198252efc60bae0860
Created unity Project <nrhymer@students.kennesaw.edu>
2021-02-23 00:43:24 -0500

    starting to creating Search functionality..RR

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import RecommendedVideos from "./RecommendedVideos";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
 
@@ -10,13 +11,24 @@ function App() {
   return (
     // BEM class naiming convention
     <div className="App">
-    {/* <h1> Building Youtube clone</h1> */}
-    <Header />
+
+    <Router>
+      <Header />
+      <Switch>
+      <Route path="/search/:searchTerm">
+        <h1>Search page </h1>
+      </Route>
+      <Route path="/">
     <div className="app__page">
       <Sidebar />
       <RecommendedVideos />
-    
     </div>
+      </Route>
+      </Switch>
+    </Router>
+
+    {/* <h1> Building Youtube clone</h1> */}
+    
     </div>
   );
 }
